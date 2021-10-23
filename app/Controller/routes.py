7da433953f895006b7884de9966fc15332e19459
Boxiang Lin <boxiang.lin@wsu.edu>
2021-10-23 15:23:18 -0700

    enviorment setup

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -3,3 +3,7 @@ from config import Config
 
 bp_routes = Blueprint('routes', __name__)
 bp_routes.template_folder = Config.TEMPLATE_FOLDER #'..\\View\\templates'
+
+@bp_routes.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
