ce9f62be36df01529d048ae57d1e4ac80f90dd3b
HongLouWang <shutian.wang@wsu.edu>
2021-10-25 16:30:12 -0700

    Task TODO Define Flask Form for the research position post

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -1,1 +1,20 @@
+from operator import length_hint
+from flask_wtf import FlaskForm
+from wtforms.fields.core import StringField
+from wtforms.validators import  DataRequired, Email, EqualTo, ValidationError, Length, DataRequired, regexp
+
+class ResearchPositionForm(FlaskForm):
+    research_title = StringField('Research Project Title', [Length(min=0, max=2048)], validators=[DataRequired()])
+    desc = StringField('Project Brif Description', [Length(min=0, max=2048)], validators=[DataRequired()])
+    start_end_date = StringField('Start date and end date(use / to seprate)', [Length(min=0, max=128)], validators=[DataRequired()])
+    time_commitment = StringField('Required Time Commitment',[Length(min=0, max=128)], validators=[DataRequired()] )
+    research_field = StringField('Research Field', [Length(min=0, max=128)], validators=[DataRequired()])
+    applicant_qualification = StringField('Applicant Qualification', [Length(min=0, max=128)], validators=[DataRequired()])
+
+    def validate_start_end_date(form, start_end_date):
+        if start_end_date.find('/'):
+            pass
+        else:
+            raise ValidationError('This field must contain / as a spliter to sperate the start date and end date.')
+
 
