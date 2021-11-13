605c513de25220550735abe9876787e1e97cf0c8
Boxiang Lin <boxiang.lin@wsu.edu>
2021-11-13 14:06:24 -0800

    modify the database model, added associatation tables, added hardcoded programming languages and research topics

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import  DataRequired, Email, EqualTo, ValidationError, Length, DataRequired, regexp
 
+
 '''
 Flaskform for faculty use - Research position post
 '''
--- app/Model/models.py
+++ app/Model/models.py
@@ -3,10 +3,35 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+'''
+Student to topic association table.
+'''
+StudentResearchTopics = db.Table('studentresearchtopics',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('topic_id', db.Integer, db.ForeignKey('researchtopic.id'))
+)
+
+
+'''
+Student to elective association table.
+'''
+StudentElectives = db.Table('studentelectives',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('technicalelectives.id'))
+)
+
+'''
+Student to Programming language association table.
+'''
+StudentLanguages = db.Table('studentlanguages',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('language_id', db.Integer, db.ForeignKey('programminglanguages.id'))
+)
 
 
 '''
@@ -17,26 +42,44 @@ class User(db.Model, UserMixin):
     ## common info ## 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
-    lastname = db.Column(db.String(20))
-    firstname = db.Column(db.String(30))
-    wsuid = db.Column(db.Integer, unique = True)
-    phone = db.Column(db.String(20))
     email = db.Column(db.String(120), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    # usertype =student or faculty
+    usertype = db.Column(db.Integer)    #0--student, 1--faculty
+    phone = db.Column(db.String(20))
+    office = db.Column(db.String(1024))
+
+    # lastname = db.Column(db.String(20))
+    # firstname = db.Column(db.String(30))
+    # wsuid = db.Column(db.Integer, unique = True)
 
     ## additional student ##
     major = db.Column(db.String(20))
-    GPA = db.Column(db.String(5))
-    gradulation = db.Column(db.String(10))
-    elective = db.Column(db.String(300))
-    researchtopic = db.Column(db.String(30))
-    programming = db.Column(db.String(30))
-    experience = db.Column(db.String(200))
+    GPA = db.Column(db.Float(5))
+    gradulationdate = db.Column(db.DateTime)
+    research_experience = db.Column(db.String(200))
 
-    # role =student or faculty
-    role = db.Column(db.String(20))
+    elective = db.relationship(
+        'TechnicalElectives', secondary = StudentElectives,
+        primaryjoin=(StudentElectives.c.user_id == id),lazy='dynamic', overlaps='roster'
+    )
 
+    researchtopic = db.relationship(
+        'ResearchTopics', secondary = StudentResearchTopics,
+        primaryjoin=(StudentResearchTopics.c.user_id == id),lazy='dynamic', overlaps='roster'
+    )
+
+    programming = db.relationship(
+        'ProgrammingLanguages', secondary = StudentLanguages,
+        primaryjoin=(StudentLanguages.c.user_id == id),lazy='dynamic', overlaps='roster'
+    )
+    
+    # for faculty use
     position = db.relationship('Position', backref='writer', lazy='dynamic')
+    
+    
+    # for student use
+    applications = db.relationship('Applications', back_populates = 'myapplications')
     
     def __repr__(self):
         return '<User {}, {}>'.format(self.id,self.username)
@@ -49,7 +92,22 @@ class User(db.Model, UserMixin):
 
     def get_role(self, role):
         return role
+    
+    
+'''
+Application model.
+'''
+class Applications(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    position_id = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+    status = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+
+'''
+Research position model.
+'''
 class Position(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(2048))
@@ -59,5 +117,33 @@ class Position(db.Model):
     time_commitment = db.Column(db.String(128))
     research_field = db.Column(db.String(128))
     applicant_qualification = db.Column(db.String(1024))
-
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    researchtopic = db.relationship(
+        'ResearchTopics', secondary = StudentResearchTopics,
+        primaryjoin=(StudentResearchTopics.c.user_id == id),lazy='dynamic', overlaps='roster'
+    )
+    
+    applications = db.relationship('Application', backref='application', lazy='dynamic')
+    
+
+'''
+Research topics model.
+'''
+class ResearchTopics(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(128))
+
+'''
+Technical Electives model.
+'''
+class TechnicalElectives(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(1024))
+
+'''
+Programming langugages model.
+'''
+class ProgrammingLanguages(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
