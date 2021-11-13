010cd9f2e53ed8364cd2ae85d6788d5286b8992c
luuis1234567 <89602757+luuis1234567@users.noreply.github.com>
2021-11-12 20:14:21 -0800

    change the model

--- app/Model/models.py
+++ app/Model/models.py
@@ -17,24 +17,38 @@ class User(db.Model, UserMixin):
     ## common info ## 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
-    lastname = db.Column(db.String(20))
-    firstname = db.Column(db.String(30))
-    wsuid = db.Column(db.Integer, unique = True)
-    phone = db.Column(db.String(20))
     email = db.Column(db.String(120), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    # usertype =student or faculty
+    usertype = db.Column(db.Integer)    #0--student, 1--faculty
+    phone = db.Column(db.String(20))
+    office = db.Column(db.String(1024))
+
+    # lastname = db.Column(db.String(20))
+    # firstname = db.Column(db.String(30))
+    # wsuid = db.Column(db.Integer, unique = True)
 
     ## additional student ##
     major = db.Column(db.String(20))
-    GPA = db.Column(db.String(5))
-    gradulation = db.Column(db.String(10))
-    elective = db.Column(db.String(300))
-    researchtopic = db.Column(db.String(30))
-    programming = db.Column(db.String(30))
-    experience = db.Column(db.String(200))
+    GPA = db.Column(db.Float(5))
+    gradulationdate = db.Column(db.DateTime)
+    research_experience = db.Column(db.String(200))
 
-    # role =student or faculty
-    role = db.Column(db.String(20))
+    elective = db.relationship(
+        'TechnicalElectives', secondary = StudentElectives,
+        primaryjoin=(StudentElectives.c.user_id == id),lazy='dynamic', overlaps='roster'
+    )
+
+    researchtopic = db.relationship(
+        'ResearchTopics', secondary = StudentResearchTopics,
+        primaryjoin=(StudentResearchTopics.c.user_id == id),lazy='dynamic', overlaps='roster'
+    )
+
+    programming = db.relationship(
+        'ProgrammingLanguages', secondary = StudentLanguages,
+        primaryjoin=(StudentLanguages.c.user_id == id),lazy='dynamic', overlaps='roster'
+    )
+    
 
     position = db.relationship('Position', backref='writer', lazy='dynamic')
     
@@ -50,6 +64,12 @@ class User(db.Model, UserMixin):
     def get_role(self, role):
         return role
 
+class Applications:
+    id = db.Column(db.Integer, primary_key = True)
+    position_id = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+    status = db.Column(db.Integer)
+
 class Position(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(2048))
@@ -59,5 +79,9 @@ class Position(db.Model):
     time_commitment = db.Column(db.String(128))
     research_field = db.Column(db.String(128))
     applicant_qualification = db.Column(db.String(1024))
-
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    researchtopic = db.relationship(
+        'ResearchTopics', secondary = StudentResearchTopics,
+        primaryjoin=(StudentResearchTopics.c.user_id == id),lazy='dynamic', overlaps='roster'
+    )
