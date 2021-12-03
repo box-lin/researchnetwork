d999f7c0c3738a0fd707ac9cb3b58901c8d1d5fc
HongLouWang <shutian.wang@wsu.edu>
2021-12-02 23:09:50 -0800

    implement hire/reject function to faculty
    implement hire/reject function to faculty

--- app/Model/models.py
+++ app/Model/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -166,6 +167,7 @@ class User(db.Model, UserMixin):
 Association object <Apply> position as <Student>.
 '''
 class Apply(db.Model):
+    id = db.Column(db.Integer, autoincrement=True)
     studentid = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True) 
     positionid = db.Column(db.Integer, db.ForeignKey('position.id'), primary_key=True)
     applydate = db.Column(db.DateTime, default = datetime.utcnow)
